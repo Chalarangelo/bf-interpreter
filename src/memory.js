9121ff6de61e5ef9b9b03462523b6796ba85926d
Angelos Chalaris <chalarangelo@gmail.com>
2025-02-12 21:14:12 +0200

    Update Memory module

--- src/memory.js
+++ src/memory.js
@@ -1,8 +1,34 @@
 class Memory {
-  constructor() {
+  constructor(initialMemory) {
     this.left = [];
     this.right = [];
     this.pointer = 0;
+
+    if (typeof initialMemory === 'undefined' || initialMemory === null) return;
+    if (typeof initialMemory === 'string') {
+      initialMemory
+        .split('')
+        .forEach((value, index) => this.set(index, value.charCodeAt(0)));
+      return;
+    }
+    if (Array.isArray(initialMemory)) {
+      initialMemory.forEach((value, index) => this.set(index, value));
+      return;
+    }
+
+    if (typeof initialMemory === 'object') {
+      const keys = Object.keys(initialMemory);
+      if (
+        keys.includes('left') &&
+        keys.includes('right') &&
+        keys.includes('pointer')
+      ) {
+        this.left = [...memory.left];
+        this.right = [...memory.right];
+        this.pointer = memory.pointer;
+        return;
+      }
+    }
   }
 
   get(index) {
@@ -11,8 +37,30 @@ class Memory {
   }
 
   set(index, value) {
-    if (index >= 0) this.right[index] = value % 256;
-    else this.left[-index - 1] = value % 256;
+    if (index >= 0) this.#setRight(index, value);
+    else this.#setLeft(-index - 1, value);
+  }
+
+  toString() {
+    const leftLength = this.left.length;
+    const memory = [...this.left, ...this.right]
+      .map((v, i) => {
+        const value = v || 0;
+        const pointer = i < leftLength ? -leftLength + i : i - leftLength;
+        const characterMap = value => {
+          if (value === 9) return '\\t';
+          if (value === 10) return '\\n';
+          if (value === 13) return '\\r';
+          if (value < 32) return null;
+          if (value > 126) return null;
+          return String.fromCharCode(value);
+        };
+        const string = characterMap(value);
+        return `${pointer}: ${value}${string ? ` (${string})` : ''}`;
+      })
+      .join(', ');
+
+    return `[${memory}]`;
   }
 
   movePointer(offset) {
@@ -24,9 +72,17 @@ class Memory {
     return this.left[index];
   }
 
+  #setLeft(index, value) {
+    this.left[index] = Math.min(Math.max(value, 0), 255);
+  }
+
   #getRight(index) {
     if (this.right[index] === undefined) this.right[index] = 0;
     return this.right[index];
+  }
+
+  #setRight(index, value) {
+    this.right[index] = Math.min(Math.max(value, 0), 255);
   }
 }
 
