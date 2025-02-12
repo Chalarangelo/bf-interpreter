6935b8bd389082f8f6509861746cb29671cbb517
Angelos Chalaris <chalarangelo@gmail.com>
2025-02-12 21:16:50 +0200

    Remove redundant nodes

--- src/ast/nodes/printValueNode.js
+++
@@ -1,9 +0,0 @@
-import ASTNode from '../astNode.js';
-
-class PrintValueNode extends ASTNode {
-  execute(memory, stdin, stdout) {
-    stdout.write(String.fromCharCode(memory.get(memory.pointer)));
-  }
-}
-
-export default PrintValueNode;
